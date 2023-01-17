469eb329f0a3729ebb1f254e1212e61ac0131575
Ronald Amiquero <roamz.2017@gmail.com>
2023-01-17 17:49:41 -0500

    [add] database port in configGlobal file

--- src/config/configGlobal.js
+++ src/config/configGlobal.js
@@ -3,6 +3,7 @@ module.exports = {
    port: process.env.PORT || 3002,
    host: process.env.HOST || 'http://localhost',
    dbHost: process.env.DB_HOST || 'localhost',
+   dbPort: process.env.DB_PORT || 3306,
    dbUser: process.env.DB_USER || 'root',
    dbPassword: process.env.DB_PASSWORD || 'CH3ATM1999',
    dbName: process.env.DB_NAME || 'db_sacha'
--- src/store/mysql.js
+++ src/store/mysql.js
@@ -3,6 +3,7 @@ const config = require('../config/configGlobal')
 
 const pool = mysql.createPool({
    host: config.dbHost,
+   port: config.dbPort,
    user: config.dbUser,
    password: config.dbPassword,
    database: config.dbName,
