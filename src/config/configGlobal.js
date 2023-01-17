b507f720ec8628b2c93461267a91bb2f25cb79ba
Ronald Amiquero <roamz.2017@gmail.com>
2023-01-17 16:25:42 -0500

    [change] package.json config

--- src/config/configGlobal.js
+++ src/config/configGlobal.js
@@ -3,7 +3,7 @@ module.exports = {
    port: process.env.PORT || 3002,
    host: process.env.HOST || 'http://localhost',
    dbHost: process.env.DB_HOST || 'localhost',
-   dbUser: process.env.DB_USER || 'roo',
+   dbUser: process.env.DB_USER || 'root',
    dbPassword: process.env.DB_PASSWORD || 'CH3ATM1999',
    dbName: process.env.DB_NAME || 'db_sacha'
 }
